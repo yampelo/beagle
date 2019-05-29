e9b0ede83d4c97fb4c52c884ba51ffe25961cd0b
yampelo <omeryampel@gmail.com>
2019-05-29 17:07:08 -0400

    process subjects

--- beagle/datasources/darpa_tc_json.py
+++ beagle/datasources/darpa_tc_json.py
@@ -1,6 +1,7 @@
 # Datasource to support the "Transparent Computing Engagement" dataset
 # https://github.com/darpa-i2o/Transparent-Computing
 import os
+from typing import Generator
 
 from beagle.datasources.base_datasource import JSONDataSource
 from beagle.transformers import DRAPATCTransformer
@@ -17,3 +18,26 @@ class DARPATCJson(JSONDataSource):
 
     def metadata(self) -> dict:
         return {"filename": os.path.basename(self.file_path)}
+
+    def events(self) -> Generator[dict, None, None]:
+        """Events are in the format:
+
+        "datum": {
+            "com.bbn.tc.schema.avro.cdm18.Subject": {
+             ...
+        }
+
+        This pops out the relevant info under the first key.
+        """
+        i = 0
+        for event in super().events():
+            event = event["datum"]
+
+            for key, data in event.items():
+                if "com.bbn.tc.schema.avro.cdm18." in key:
+                    data["event_type"] = key.split("com.bbn.tc.schema.avro.cdm18.")[-1].lower()
+                    yield data
+                    break
+            i += 1
+            if i > 20000:
+                break
--- beagle/transformers/darpa_tc_transformer.py
+++ beagle/transformers/darpa_tc_transformer.py
@@ -1,5 +1,18 @@
+from typing import List, Optional, Tuple, Union
+
 from beagle.common import logger
+from beagle.nodes import Process
 from beagle.transformers.base_transformer import Transformer
+
+
+# Custom Node classes to use the UUID in TC
+class TCProcess(Process):
+    key_fields: List[str] = ["uuid"]
+    uuid: Optional[str]
+
+    def __init__(self, uuid: str = None, *args, **kwargs) -> None:
+        self.uuid = uuid
+        super().__init__(*args, **kwargs)
 
 
 class DRAPATCTransformer(Transformer):
@@ -10,4 +23,30 @@ class DRAPATCTransformer(Transformer):
 
         super().__init__(*args, **kwargs)
 
-        logger.info("Created Windows EVTX Transformer.")
+        logger.info("Created Darpa Transperant Computing Transformer.")
+
+    def transform(self, event: dict) -> Optional[Tuple]:
+        event_type = event["event_type"]
+
+        if event_type == "subject" and event["type"] == "SUBJECT_PROCESS":
+            return self.make_subject(event)
+
+        return tuple()
+
+    def make_subject(self, event: dict) -> Union[Tuple[TCProcess], Tuple[TCProcess, TCProcess]]:
+
+        if event.get("cmdLine"):
+            proc_cmdline = event["cmdLine"]["string"]
+        else:
+            proc_cmdline = None
+
+        proc = TCProcess(uuid=event["uuid"], process_image=proc_cmdline, command_line=proc_cmdline)
+        if event.get("parentSubject"):
+
+            parent = TCProcess(uuid=event["parentSubject"]["com.bbn.tc.schema.avro.cdm18.UUID"])
+
+            parent.launched[proc]
+
+            return (proc, parent)
+        else:
+            return (proc,)
