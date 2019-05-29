a3d065c7815cc5a43ef9d402bd178ccb511b9efe
yampelo <omeryampel@gmail.com>
2019-05-29 16:31:55 -0400

    fixes imports

--- beagle/nodes/__init__.py
+++ beagle/nodes/__init__.py
@@ -1,11 +1,25 @@
 from __future__ import absolute_import
 
-from .node import Node  # noqa:F401
-from .edge import Edge  # noqa:F401
+from .node import Node
+from .edge import Edge
 
-from .domain import URI, Domain  # noqa:F401
-from .file import File, FileOf  # noqa:F401
-from .ip_address import IPAddress  # noqa:F401
-from .process import Launched, Process  # noqa:F401
-from .registry import RegistryKey  # noqa:F401
-from .alert import Alert  # noqa:F401
+from .domain import URI, Domain
+from .file import File, FileOf
+from .ip_address import IPAddress
+from .process import Launched, Process
+from .registry import RegistryKey
+from .alert import Alert
+
+__all__ = [
+    "Node",
+    "Edge",
+    "URI",
+    "Domain",
+    "File",
+    "FileOf",
+    "IPAddress",
+    "Launched",
+    "Process",
+    "RegistryKey",
+    "Alert",
+]
