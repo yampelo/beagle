420450ccfe773097bf666735503f5ddde2421d19
yampelo <omeryampel@gmail.com>
2019-05-29 19:37:29 -0400

    adds file and process tests

--- beagle/transformers/darpa_tc_transformer.py
+++ beagle/transformers/darpa_tc_transformer.py
@@ -1,12 +1,21 @@
 from typing import List, Optional, Tuple, Union
 
-from beagle.common import logger
-from beagle.nodes import Process
+from beagle.common import logger, split_path
+from beagle.nodes import File, Process
 from beagle.transformers.base_transformer import Transformer
 
 
 # Custom Node classes to use the UUID in TC
 class TCProcess(Process):
+    key_fields: List[str] = ["uuid"]
+    uuid: Optional[str]
+
+    def __init__(self, uuid: str = None, *args, **kwargs) -> None:
+        self.uuid = uuid
+        super().__init__(*args, **kwargs)
+
+
+class TCFile(File):
     key_fields: List[str] = ["uuid"]
     uuid: Optional[str]
 
@@ -29,24 +38,62 @@ class DRAPATCTransformer(Transformer):
         event_type = event["event_type"]
 
         if event_type == "subject" and event["type"] == "SUBJECT_PROCESS":
-            return self.make_subject(event)
+            return self.make_process(event)
+        if event_type == "fileobject" and event["type"] == "FILE_OBJECT_BLOCK":
+            return self.make_file(event)
 
         return tuple()
 
-    def make_subject(self, event: dict) -> Union[Tuple[TCProcess], Tuple[TCProcess, TCProcess]]:
-
+    def make_process(self, event: dict) -> Union[Tuple[TCProcess], Tuple[TCProcess, TCProcess]]:
         if event.get("cmdLine"):
             proc_cmdline = event["cmdLine"]["string"]
         else:
             proc_cmdline = None
 
-        proc = TCProcess(uuid=event["uuid"], process_image=proc_cmdline, command_line=proc_cmdline)
+        path = None
+        image = None
+        if event.get("properties"):
+            path = event["properties"]["map"].get("path")
+            if "/" in path:
+                # Swap the path directions
+                path = path.replace("/", "\\")
+            image, path = split_path(path)
+
+        proc = TCProcess(
+            uuid=event["uuid"],
+            process_image=image or proc_cmdline,
+            process_image_path=path or proc_cmdline,
+            command_line=proc_cmdline,
+            host=event["hostId"],
+        )
         if event.get("parentSubject"):
 
-            parent = TCProcess(uuid=event["parentSubject"]["com.bbn.tc.schema.avro.cdm18.UUID"])
+            parent = TCProcess(
+                uuid=event["parentSubject"]["com.bbn.tc.schema.avro.cdm18.UUID"],
+                host=event["hostId"],
+            )
 
             parent.launched[proc]
 
             return (proc, parent)
         else:
             return (proc,)
+
+    def make_file(self, event: dict) -> Tuple[TCFile]:
+
+        base_obj = event["baseObject"]
+
+        file_node = TCFile(uuid=event["uuid"], host=base_obj["hostId"])
+
+        # Since not everything has a full path, and this is multiple different systems,
+        # this is the best try for this.
+        if base_obj.get("properties"):
+            full_path = base_obj["properties"]["map"].get("filename", "")
+            full_path = full_path.replace("/", "\\")
+            file_name, file_path = split_path(full_path)
+
+            file_node.full_path = full_path
+            file_node.file_path = file_path
+            file_node.file_name = file_name
+
+        return (file_node,)
---
+++ tests/transformers/test_darpa_tc_transformer.py
@@ -0,0 +1,170 @@
+import pytest
+
+from beagle.transformers import DRAPATCTransformer
+from beagle.transformers.darpa_tc_transformer import TCFile, TCProcess
+
+
+@pytest.fixture
+def transformer() -> DRAPATCTransformer:
+    return DRAPATCTransformer(datasource=None)  # type: ignore
+
+
+def test_make_process(transformer):
+    test_event = {
+        "event_type": "subject",
+        "uuid": "B80F3806-0000-0000-0000-000000000020",
+        "type": "SUBJECT_PROCESS",
+        "cid": 4024,
+        "parentSubject": {
+            "com.bbn.tc.schema.avro.cdm18.UUID": "52071700-0000-0000-0000-000000000020"
+        },
+        "hostId": "0A00063C-5254-00F0-0D60-000000000070",
+        "localPrincipal": "EC000000-0000-0000-0000-000000000060",
+        "startTimestampNanos": 1522943310819901200,
+        "unitId": None,
+        "iteration": None,
+        "count": None,
+        "cmdLine": {"string": "/usr/bin/firefox"},
+        "privilegeLevel": None,
+        "importedLibraries": None,
+        "exportedLibraries": None,
+        "properties": {
+            "map": {"tgid": "3934", "path": "/home/admin/Downloads/firefox/firefox", "ppid": "1874"}
+        },
+    }
+
+    nodes = transformer.transform(test_event)
+
+    proc: TCProcess = nodes[0]
+    parent: TCProcess = nodes[1]
+    assert proc.uuid == "B80F3806-0000-0000-0000-000000000020"
+    assert proc.command_line == "/usr/bin/firefox"
+    assert proc.process_image == "firefox"
+    assert parent.uuid == "52071700-0000-0000-0000-000000000020"
+
+
+def test_make_process_no_properties(transformer):
+    test_event = {
+        "event_type": "subject",
+        "uuid": "B80F3806-0000-0000-0000-000000000020",
+        "type": "SUBJECT_PROCESS",
+        "cid": 4024,
+        "parentSubject": {
+            "com.bbn.tc.schema.avro.cdm18.UUID": "52071700-0000-0000-0000-000000000020"
+        },
+        "hostId": "0A00063C-5254-00F0-0D60-000000000070",
+        "localPrincipal": "EC000000-0000-0000-0000-000000000060",
+        "startTimestampNanos": 1522943310819901200,
+        "unitId": None,
+        "iteration": None,
+        "count": None,
+        "cmdLine": {"string": "/usr/bin/firefox"},
+        "privilegeLevel": None,
+        "importedLibraries": None,
+        "exportedLibraries": None,
+        "properties": None,
+    }
+
+    nodes = transformer.transform(test_event)
+
+    proc: TCProcess = nodes[0]
+    parent: TCProcess = nodes[1]
+    assert proc.uuid == "B80F3806-0000-0000-0000-000000000020"
+    assert proc.command_line == "/usr/bin/firefox"
+    assert proc.process_image == "/usr/bin/firefox"
+    assert parent.uuid == "52071700-0000-0000-0000-000000000020"
+
+
+def test_make_process_no_parent(transformer):
+    test_event = {
+        "event_type": "subject",
+        "uuid": "B80F3806-0000-0000-0000-000000000020",
+        "type": "SUBJECT_PROCESS",
+        "cid": 4024,
+        "parentSubject": None,
+        "hostId": "0A00063C-5254-00F0-0D60-000000000070",
+        "localPrincipal": "EC000000-0000-0000-0000-000000000060",
+        "startTimestampNanos": 1522943310819901200,
+        "unitId": None,
+        "iteration": None,
+        "count": None,
+        "cmdLine": {"string": "/usr/bin/firefox"},
+        "privilegeLevel": None,
+        "importedLibraries": None,
+        "exportedLibraries": None,
+        "properties": None,
+    }
+
+    nodes = transformer.transform(test_event)
+    assert len(nodes) == 1
+
+
+def test_make_file(transformer):
+    test_event = {
+        "uuid": "0100D00F-1400-2E00-0000-00004FA79C38",
+        "event_type": "fileobject",
+        "baseObject": {
+            "hostId": "0A00063C-5254-00F0-0D60-000000000070",
+            "permission": None,
+            "epoch": None,
+            "properties": {
+                "map": {
+                    "dev": "265289729",
+                    "inode": "3014676",
+                    "filename": "/home/admin/.cache/mozilla/firefox/pe11scpa.default/thumbnails/31017840be38acf6baf0e8f850d5c94b.png",
+                }
+            },
+        },
+        "type": "FILE_OBJECT_BLOCK",
+        "fileDescriptor": None,
+        "localPrincipal": {
+            "com.bbn.tc.schema.avro.cdm18.UUID": "EC000000-0000-0000-0000-000000000060"
+        },
+        "size": None,
+        "peInfo": None,
+        "hashes": None,
+    }
+    nodes = transformer.transform(test_event)
+    assert len(nodes) == 1
+
+    fnode: TCFile = nodes[0]
+    assert fnode.uuid == "0100D00F-1400-2E00-0000-00004FA79C38"
+    assert fnode.host == "0A00063C-5254-00F0-0D60-000000000070"
+    assert (
+        fnode.full_path
+        == "\\home\\admin\\.cache\\mozilla\\firefox\\pe11scpa.default\\thumbnails\\31017840be38acf6baf0e8f850d5c94b.png"
+    )
+    assert fnode.file_name == "31017840be38acf6baf0e8f850d5c94b.png"
+    assert (
+        fnode.file_path == "\\home\\admin\\.cache\\mozilla\\firefox\\pe11scpa.default\\thumbnails"
+    )
+
+
+def test_make_file_no_properties(transformer):
+    test_event = {
+        "uuid": "0100D00F-1400-2E00-0000-00004FA79C38",
+        "event_type": "fileobject",
+        "baseObject": {
+            "hostId": "0A00063C-5254-00F0-0D60-000000000070",
+            "permission": None,
+            "epoch": None,
+            "properties": None,
+        },
+        "type": "FILE_OBJECT_BLOCK",
+        "fileDescriptor": None,
+        "localPrincipal": {
+            "com.bbn.tc.schema.avro.cdm18.UUID": "EC000000-0000-0000-0000-000000000060"
+        },
+        "size": None,
+        "peInfo": None,
+        "hashes": None,
+    }
+    nodes = transformer.transform(test_event)
+    assert len(nodes) == 1
+
+    fnode: TCFile = nodes[0]
+    assert fnode.uuid == "0100D00F-1400-2E00-0000-00004FA79C38"
+    assert fnode.host == "0A00063C-5254-00F0-0D60-000000000070"
+    assert fnode.full_path == ""
+    assert fnode.file_name is None
+    assert fnode.file_path is None
