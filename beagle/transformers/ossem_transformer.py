183f68b6f065443637c6fc2ae0166e8fdbbe4810
yampelo <omeryampel@gmail.com>
2019-06-02 22:10:30 -0400

    Adds file events

--- beagle/transformers/ossem_transformer.py
+++ beagle/transformers/ossem_transformer.py
@@ -6,8 +6,11 @@ from beagle.transformers.base_transformer import Transformer
 
 
 class OSSEMTransformer(Transformer):
-    """Transformer based on the formats defined here:
+    """Transformer based on the fields defined here:
     https://github.com/Cyb3rWard0g/OSSEM/tree/master/common_information_model
+
+    And the relationships defined here:
+    https://docs.google.com/spreadsheets/d/1ow7YRDEDJs67kcKMZZ66_5z1ipJry9QrsDQkjQvizJM/edit#gid=0
 
     Parameters
     ----------
@@ -27,8 +30,10 @@ class OSSEMTransformer(Transformer):
 
         relationship = event["event_type"]
 
-        if relationship == "created":
+        if relationship == "launched":
             return self.created(event)
+        elif relationship in ["loaded", "created", "modified", "downloaded"]:
+            return self.file_ops(event)
 
         return tuple()
 
@@ -61,3 +66,27 @@ class OSSEMTransformer(Transformer):
         parent.launched[proc].append(timestamp=event["event_creation_time"])
 
         return (proc, proc_file, parent, parent_file)
+
+    def file_ops(self, event: dict) -> Tuple[Process, File, File]:
+        proc_name, proc_path = split_path(event["process_path"])
+
+        proc = Process(
+            process_id=event.get("process_id"),
+            process_image=proc_name,
+            process_image_path=proc_path,
+            command_line=event.get("process_command_line"),
+        )
+        proc_file = proc.get_file_node()
+
+        proc_file.file_of[proc]
+
+        name, path = split_path(event["file_path"])
+
+        dest_file = File(file_name=name, file_path=path, extension=event.get("file_extension"))
+
+        if event["event_type"] == "loaded":
+            proc.loaded[dest_file].append(timestamp=event["event_creation_time"])
+        elif event["event_type"] in ["created", "modified", "downloaded"]:
+            proc.wrote[dest_file].append(timestamp=event["event_creation_time"])
+
+        return (proc, proc_file, dest_file)
--- tests/transformers/test_ossem_transformer.py
+++ tests/transformers/test_ossem_transformer.py
@@ -16,7 +16,7 @@ def test_unknown_event(transformer):
 def test_make_process(transformer):
     test_event = {
         "event_creation_time": "1",
-        "event_type": "created",
+        "event_type": "launched",
         "process_id": "4756",
         "process_name": "conhost.exe",
         "process_path": "C:\\Windows\\System32\\conhost.exe",
@@ -51,3 +51,39 @@ def test_make_process(transformer):
     assert parent_proc.process_image == "cmd.exe"
     assert parent_proc.process_image_path == "C:\\Windows\\System32"
     assert parent_proc.command_line == "C:\\WINDOWS\\system32\\cmd.exe"
+
+
+@pytest.mark.parametrize(
+    "eventtype,attribute",
+    [("loaded", "loaded"), ("created", "wrote"), ("modified", "wrote"), ("downloaded", "wrote")],
+)
+def test_file_operations(transformer, eventtype, attribute):
+    test_event = {
+        "event_creation_time": "1",
+        "event_type": eventtype,
+        "process_id": "4756",
+        "process_name": "conhost.exe",
+        "process_path": "C:\\Windows\\System32\\conhost.exe",
+        "process_command_line": "??\\C:\\WINDOWS\\system32\\conhost.exe 0xffffffff -ForceV1",
+        "file_name": "cmd.exe",
+        "file_path": "C:\\Windows\\System32\\cmd.exe",
+    }
+    nodes = transformer.transform(test_event)
+
+    assert len(nodes) == 3
+
+    proc: Process = nodes[0]
+    proc_file: File = nodes[1]
+    dest_file: File = nodes[2]
+
+    assert proc in proc_file.file_of
+
+    assert proc.process_id == "4756"
+    assert proc.process_image == "conhost.exe"
+    assert proc.process_image_path == "C:\\Windows\\System32"
+    assert proc.command_line == "??\\C:\\WINDOWS\\system32\\conhost.exe 0xffffffff -ForceV1"
+
+    assert dest_file.file_name == "cmd.exe"
+    assert dest_file.file_path == "C:\\Windows\\System32"
+
+    assert {"timestamp": "1"} in getattr(proc, attribute)[dest_file]
