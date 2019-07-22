d22e18f003b7874aa84cd545f9b6ded05d37e923
yampelo <omeryampel@gmail.com>
2019-07-22 14:27:16 +0800

    adds default param to views

--- beagle/web/api/views.py
+++ beagle/web/api/views.py
@@ -62,6 +62,7 @@ SCHEMA = {
                 {
                     "name": k,
                     "required": (v.default == _empty),
+                    "default": (v.default),
                 }  # Check if there is a default value, if not, required.
                 for k, v in inspect.signature(
                     datasource
