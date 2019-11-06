4d046bcef1fea30814da2f00c3dcdf9a0fceb8ff
yampelo <omeryampel@gmail.com>
2019-11-06 17:05:21 -0500

    Moves host fields to IPAddress fields

--- beagle/nodes/ip_address.py
+++ beagle/nodes/ip_address.py
@@ -1,6 +1,8 @@
-from typing import List, Optional
+from collections import defaultdict
+from typing import DefaultDict, List, Optional
 
-from beagle.nodes import Node
+from beagle.nodes import Node, URI, Domain
+from beagle.nodes.process import ConnectedTo, DNSQueryFor, HTTPRequestTo
 
 
 class IPAddress(Node):
@@ -9,11 +11,22 @@ class IPAddress(Node):
     __color__ = "#87CEEB"
 
     ip_address: Optional[str]
+    mac: Optional[str]
 
     key_fields: List[str] = ["ip_address"]
 
-    def __init__(self, ip_address: str = None):
+    connected_to: DefaultDict["IPAddress", ConnectedTo]
+    http_request_to: DefaultDict[URI, HTTPRequestTo]
+    dns_query_for: DefaultDict[Domain, DNSQueryFor]  # List of DNS Lookups
+
+    def __init__(self, ip_address: str = None, mac: str = None):
         self.ip_address = ip_address
+        self.mac = mac
+
+        self.connected_to = defaultdict(ConnectedTo)
+
+        self.http_request_to = defaultdict(HTTPRequestTo)
+        self.dns_query_for = defaultdict(DNSQueryFor)
 
     @property
     def _display(self) -> str:
