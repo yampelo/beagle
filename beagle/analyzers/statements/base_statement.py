3cbae4f254f532b274f880e8d49da0660632f944
yampelo <omeryampel@gmail.com>
2019-11-15 01:53:48 -0500

    EdgeByProps: Adds statement to return subgraph that contains a matching edge.

--- beagle/analyzers/statements/base_statement.py
+++ beagle/analyzers/statements/base_statement.py
@@ -13,10 +13,28 @@ class Statement(object):
 
 class NodeByProps(Statement):
     def __init__(self, node_type: Type[Node], props: Dict[str, FieldLookup]):
+        """Searches the graph for a node of type `node_type` with properties matching `props`
+
+        Parameters
+        ----------
+        node_type : Type[Node]
+            The type of node to look for. e.g. Process
+        props : Dict[str, FieldLookup]
+            The set of props to filter the resulting nodes by.
+
+        Examples
+        ----------
+        Filter for Process nodes, with command lines that contain `text.exe`
+        >>> NodeByProps(node_type=Process, props={"command_line": Contains("test.exe")})
+
+        """
         self.node_type = node_type
         self.props = props
 
     def execute_networkx(self, G: nx.Graph) -> nx.Graph:
+        """Searches a `nx.Graph` object for nodes that match type `node_type` and contains
+        props matching `props`. This is O(V).
+        """
         subgraph_nodes = []
 
         # For each node
@@ -30,3 +48,54 @@ class NodeByProps(Statement):
                     subgraph_nodes.append(node_id)
 
         return G.subgraph(subgraph_nodes)
+
+
+class EdgeByProps(Statement):
+    def __init__(self, edge_type: str, props: Dict[str, FieldLookup]):
+        """Searches the graph for an edge of type `edge_type` with properties matching `props`
+
+        Parameters
+        ----------
+        edge_type : str
+            The type of edge to look for. e.g. Wrote
+        props : Dict[str, FieldLookup]
+            The set of props to filter the resulting edges by.
+
+        Examples
+        ----------
+        Filter for TCP edges, with contents that match ".pdf"
+        >>> EdgeByProps(edge_type="TCP", props={"payload": Contains(".pdf")})
+
+        """
+        self.edge_type = edge_type
+        self.props = props
+
+    def execute_networkx(self, G: nx.Graph) -> nx.Graph:
+        """Searches a `nx.Graph` object for edges that match type `edge_type` and contains
+        props matching `props`. This is O(E).
+
+        Returns a subgraph with all nodes contained in match edges
+        """
+        subgraph_edges = []
+
+        # For each edge
+        for u, v, k, e_data in G.edges(data=True, keys=True):
+
+            # pull out the data field from NX
+            data = e_data["data"]  # edge data
+            e_type = e_data["edge_name"]  # edge type
+
+            # If edge matches the desired instance.
+            if e_type == self.edge_type:
+
+                # Test the edge
+                if not isinstance(data, list):
+                    data = [data]
+
+                for entry in data:
+                    if any([lookup.test(entry.get(prop)) for prop, lookup in self.props.items()]):
+                        subgraph_edges.append((u, v, k))
+                        # can stop on first match
+                        break
+
+        return G.edge_subgraph(subgraph_edges)
--- tests/analyzers/statements/test_base_statement.py
+++ tests/analyzers/statements/test_base_statement.py
@@ -1,8 +1,8 @@
 import pytest
 from beagle.backends.networkx import NetworkX
-from beagle.analyzers.statements.base_statement import NodeByProps
-from beagle.analyzers.statements.lookups import Contains, EndsWith, StartsWith
-from beagle.nodes.process import Process
+from beagle.analyzers.statements.base_statement import NodeByProps, EdgeByProps
+from beagle.analyzers.statements.lookups import Contains, EndsWith, StartsWith, Exact
+from beagle.nodes.process import Process, File
 
 
 @pytest.fixture
@@ -14,6 +14,33 @@ def G1():
     proc.launched[other_proc].append(timestamp=1)
 
     backend = NetworkX(consolidate_edges=True, nodes=[proc, other_proc])
+
+    return backend.graph()
+
+
+@pytest.fixture
+def G2():
+    # A basic graph, with two nodes an an edge
+    proc = Process(process_id=10, process_image="test.exe", command_line="test.exe /c foobar")
+    f = File(file_name="foo", file_path="bar")
+
+    proc.wrote[f].append(contents="foo")
+
+    backend = NetworkX(consolidate_edges=True, nodes=[proc, f])
+
+    return backend.graph()
+
+
+@pytest.fixture
+def G3():
+    # *no consolidating*
+    proc = Process(process_id=10, process_image="test.exe", command_line="test.exe /c foobar")
+    f = File(file_name="foo", file_path="bar")
+
+    proc.wrote[f].append(contents="foo")
+    proc.wrote[f].append(contents="bar")
+
+    backend = NetworkX(consolidate_edges=False, nodes=[proc, f])
 
     return backend.graph()
 
@@ -68,3 +95,23 @@ def test_node_conditional(G1):
     assert [n["data"] for _, n in statement.execute_networkx(G1).nodes(data=True)] == [
         Process(process_id=10, process_image="test.exe", command_line="test.exe /c foobar")
     ]
+
+
+def test_one_edge_prop_test(G2, G3):
+    statement = EdgeByProps(edge_type="Wrote", props={"contents": Exact("foo")})
+
+    assert [n["data"] for _, n in statement.execute_networkx(G2).nodes(data=True)] == [
+        Process(process_id=10, process_image="test.exe", command_line="test.exe /c foobar"),
+        File(file_name="foo", file_path="bar"),
+    ]
+
+    # Should work on the non-conslidating graph too.
+    assert [n["data"] for _, n in statement.execute_networkx(G3).nodes(data=True)] == [
+        Process(process_id=10, process_image="test.exe", command_line="test.exe /c foobar"),
+        File(file_name="foo", file_path="bar"),
+    ]
+
+    statement = EdgeByProps(edge_type="Launched", props={"contents": Exact("bar")})
+
+    # Should match on `proc` from G1
+    assert [n["data"] for _, n in statement.execute_networkx(G2).nodes(data=True)] == []
