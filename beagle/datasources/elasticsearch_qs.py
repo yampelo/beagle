c2dc9682de070185d6dea87f76c99b041dc1a48c
yampelo <omeryampel@gmail.com>
2019-11-23 12:57:51 -0500

    formats with black

--- beagle/datasources/elasticsearch_qs.py
+++ beagle/datasources/elasticsearch_qs.py
@@ -8,6 +8,7 @@ from beagle.config import Config
 from beagle.datasources.base_datasource import ExternalDataSource
 from beagle.transformers.generic_transformer import GenericTransformer
 
+
 class ElasticSearchQSSerach(ExternalDataSource):
     """Datasource which allows transforming the results of a Elasticsearch Query String search into a
     graph.
@@ -31,7 +32,10 @@ class ElasticSearchQSSerach(ExternalDataSource):
     name = "Elasticsearch Query String"
     transformers = [GenericTransformer]
     category = "Elasticsearch"
-    def __init__(self, index: str = "logs-*", query: str = "*",  earliest: str = "-7d", latest: str = "now"):
+
+    def __init__(
+        self, index: str = "logs-*", query: str = "*", earliest: str = "-7d", latest: str = "now"
+    ):
         """Creates a splunk query to pull data from
 
         Parameters
@@ -54,15 +58,15 @@ class ElasticSearchQSSerach(ExternalDataSource):
 
     def setup_session(self):  # pragma: no cover
         from elasticsearch import Elasticsearch
-        
+
         client_kwargs = {
             "host": Config.get("elasticsearch", "host"),
-            "scheme": Config.get("elasticsearch", "scheme"), 
+            "scheme": Config.get("elasticsearch", "scheme"),
             "port": int(Config.get("elasticsearch", "port", fallback=9200)),
         }
         if Config.get("elasticsearch", "username") and Config.get("elasticsearch", "password"):
             client_kwargs["http_auth"] = (
-                Config.get("elasticsearch", "username"),             
+                Config.get("elasticsearch", "username"),
                 Config.get("elasticsearch", "password"),
             )
 
@@ -73,46 +77,39 @@ class ElasticSearchQSSerach(ExternalDataSource):
         query = {
             "query": {
                 "bool": {
-                    "must": {
-                        "query_string": {
-                                    "query": self.query
-                        }
-                    },                   
+                    "must": {"query_string": {"query": self.query}},
                     "filter": [
                         {
                             "range": {
-                                "@timestamp": {
-                                    "gte": "now" + self.earliest,
-                                    "lte": self.latest
-                                }
+                                "@timestamp": {"gte": "now" + self.earliest, "lte": self.latest}
                             }
                         }
-                    ]
+                    ],
                 }
             }
         }
-        
-        #Number of hits to return
+
+        # Number of hits to return
         size = int(Config.get("elasticsearch", "scroll_size", fallback=100))
-        data = self.client.search(index=self.index, body=query, scroll='2m', size=size)
+        data = self.client.search(index=self.index, body=query, scroll="2m", size=size)
 
         # Get the scroll ID
-        sid = data['_scroll_id']
-        scroll_size = len(data['hits']['hits'])
+        sid = data["_scroll_id"]
+        scroll_size = len(data["hits"]["hits"])
 
         while scroll_size > 0:
             # Before scroll, process current batch of hits
-            for item in data['hits']['hits']:
-                source = item['_source']
-                source['_id'] = item["_id"]
+            for item in data["hits"]["hits"]:
+                source = item["_source"]
+                source["_id"] = item["_id"]
                 yield source
-            data = self.client.scroll(scroll_id=sid, scroll='2m')
+            data = self.client.scroll(scroll_id=sid, scroll="2m")
 
             # Update the scroll ID
-            sid = data['_scroll_id']
+            sid = data["_scroll_id"]
 
             # Get the number of results that returned in the last scroll
-            scroll_size = len(data['hits']['hits'])
+            scroll_size = len(data["hits"]["hits"])
 
     def metadata(self) -> dict:  # pragma: no cover
         return {
